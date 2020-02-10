01a4d72497c69890247b9e79f03498f917225766
jois6507 <johan.haag.6507@student.uu.se>
2020-02-10 14:17:55 +0100

    added interactive map

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,44 +1,76 @@
+'use strict';
+const socket = io();
 
 
 const vm = new Vue({
-  el: 'main',
+    el: 'main',
     data: {
 	food,
 	fullname: null,
 	email: null,
-	street: null,
-	house: null,
 	paymentmethod: '',
-	gender: 3,
+	gender: "Not Provided",
 	genderName: null,
 	missingFields: false,
-	orders: [],
+	myOrder: [],
+	orders: {},
 	customerInfoArray: null
     },
+    created: function() {
+	/* When the page is loaded, get the current orders stored on the server.
+	 * (the server's code is in app.js) */
+	socket.on('initialize', function(data) {
+	    this.orders = data.orders;
+	}.bind(this));
+
+	/* Whenever an addOrder is emitted by a client (every open map.html is
+	 * a client), the server responds with a currentQueue message (this is
+	 * defined in app.js). The message's data payload is the entire updated
+	 * order object. Here we define what the client should do with it.
+	 * Spoiler: We replace the current local order object with the new one. */
+	socket.on('currentQueue', function(data) {
+	    this.orders = data.orders;
+	}.bind(this));
+    },
     methods: {
+	getNext: function() {
+	    /* This function returns the next available key (order number) in
+	     * the orders object, it works under the assumptions that all keys
+	     * are integers. */
+	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+		return Math.max(last, next);
+	    }, 0);
+	    return lastOrder + 1;
+	},
+	addOrder: function(event) {
+	    /* When you click in the map, a click event object is sent as parameter
+	     * to the function designated in v-on:click (i.e. this one).
+	     * The click event object contains among other things different
+	     * coordinates that we need when calculating where in the map the click
+	     * actually happened. */
+	    let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		details: {
+		    x: event.clientX - 10 - offset.x,
+		    y: event.clientY - 10 - offset.y,
+		},
+		orderItems: ['Beans', 'Curry'],
+	    });
+	},
 	makeOrder: function ()
 	{
-	    if (!this.fullname || !this.email || !this.street || !this.house || !this.paymentmethod || !this.gender) {
+	    if (!this.fullname || !this.email || !this.paymentmethod || !this.gender) {
 		this.missingFields = true;
 		return;
 	    }
 	    
 	    this.missingFields = false;
 	    
-	    switch(this.gender) {
-	    case 1:
-		this.genderName = "Male";
-		break;
-	    case 2:
-		this.genderName = "Female";
-		break;
-	    case 3:
-		this.genderName = "Do not wish to provide";
-		break;
-	    }
-	    
-	    this.customerInfoArray = [this.fullname, this.email, this.street, this.house, this.paymentmethod, this.gender, this.orders];
-	    //return this.customerInfoArray;
+	    this.customerInfoArray = [this.fullname, this.email, this.paymentmethod, this.gender, this.orders];
 	}
     }
 })
