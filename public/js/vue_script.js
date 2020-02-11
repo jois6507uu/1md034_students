ce609a6ba1d31d39487790cbccbdd611a655aa43
jois6507 <johan.haag.6507@student.uu.se>
2020-02-11 18:40:49 +0100

    Added map where you can choose delivary address

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -13,34 +13,26 @@ const vm = new Vue({
 	genderName: null,
 	missingFields: false,
 	myOrder: [],
+	order: {
+	    details: {
+		x: 0,
+		y: 0
+	    }
+	},
+	counter: 0,
 	orders: {},
 	customerInfoArray: null
-    },
-    created: function() {
-	/* When the page is loaded, get the current orders stored on the server.
-	 * (the server's code is in app.js) */
-	socket.on('initialize', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
-
-	/* Whenever an addOrder is emitted by a client (every open map.html is
-	 * a client), the server responds with a currentQueue message (this is
-	 * defined in app.js). The message's data payload is the entire updated
-	 * order object. Here we define what the client should do with it.
-	 * Spoiler: We replace the current local order object with the new one. */
-	socket.on('currentQueue', function(data) {
-	    this.orders = data.orders;
-	}.bind(this));
     },
     methods: {
 	getNext: function() {
 	    /* This function returns the next available key (order number) in
 	     * the orders object, it works under the assumptions that all keys
 	     * are integers. */
-	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+	    /*let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
 		return Math.max(last, next);
 	    }, 0);
-	    return lastOrder + 1;
+	    return lastOrder + 1;*/
+	    return this.counter + 1;
 	},
 	addOrder: function(event) {
 	    /* When you click in the map, a click event object is sent as parameter
@@ -48,29 +40,55 @@ const vm = new Vue({
 	     * The click event object contains among other things different
 	     * coordinates that we need when calculating where in the map the click
 	     * actually happened. */
+
+	    /*
+	    let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };*/
+	    
+
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		
+		details: {
+		    /*x: event.clientX - 10 - offset.x,*/
+		    /*y: event.clientY - 10 - offset.y,*/
+		    x: this.order.details.x,
+		    y: this.order.details.y,
+		},
+		orderItems: this.myOrder,
+	    });
+	},
+	displayOrder: function ()
+	{
+	    /*
+	    if (!this.fullname || !this.email || !this.paymentmethod || !this.gender) {
+		this.missingFields = true;
+		
+		
+	    } else {
+		this.missingFields = false;
+		
+
+	    */
 	    let offset = {
 		x: event.currentTarget.getBoundingClientRect().left,
 		y: event.currentTarget.getBoundingClientRect().top,
 	    };
-	    socket.emit('addOrder', {
-		orderId: this.getNext(),
-		details: {
-		    x: event.clientX - 10 - offset.x,
-		    y: event.clientY - 10 - offset.y,
-		},
-		orderItems: ['Beans', 'Curry'],
-	    });
-	},
-	makeOrder: function ()
-	{
-	    if (!this.fullname || !this.email || !this.paymentmethod || !this.gender) {
-		this.missingFields = true;
-		return;
+	    
+	    this.order.details.x = event.clientX - 10 - offset.x;
+	    this.order.details.y = event.clientY - 10 - offset.y;
+	    
+	    
+	    console.log('x: ' + this.order.details.x + ', y: ' + this.order.details.y);
+		
 	    }
 	    
-	    this.missingFields = false;
 	    
-	    this.customerInfoArray = [this.fullname, this.email, this.paymentmethod, this.gender, this.orders];
-	}
+	    
+	
+	
     }
+ 
 })
