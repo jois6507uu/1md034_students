30a7ff6ea75520f4836b98d1d14c8eebbf03a4c1
jois6507 <johan.haag.6507@student.uu.se>
2020-02-12 18:40:28 +0100

    added customer information in dispatcher

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -11,28 +11,47 @@ const vm = new Vue({
 	paymentmethod: '',
 	gender: "Not Provided",
 	genderName: null,
-	missingFields: false,
+	missingFields: true,
 	myOrder: [],
-	order: {
+	localOrder: {
 	    details: {
 		x: 0,
 		y: 0
 	    }
 	},
-	counter: 0,
+	counter : 0,
 	orders: {},
 	customerInfoArray: null
     },
+    created: function() {
+    /* When the page is loaded, get the current orders stored on the server.
+     * (the server's code is in app.js) */
+    socket.on('initialize', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    /* Whenever an addOrder is emitted by a client (every open map.html is
+     * a client), the server responds with a currentQueue message (this is
+     * defined in app.js). The message's data payload is the entire updated
+     * order object. Here we define what the client should do with it.
+     * Spoiler: We replace the current local order object with the new one. */
+    socket.on('currentQueue', function(data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+	/* OBS Jag har försökt att köra utan dessa calls med 'socket.on' men då kan jag inte göra så att flera orders visas i dispatcher vyn, endast den senaste ordern visas. */
+  },
     methods: {
 	getNext: function() {
 	    /* This function returns the next available key (order number) in
 	     * the orders object, it works under the assumptions that all keys
 	     * are integers. */
-	    /*let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
 		return Math.max(last, next);
 	    }, 0);
-	    return lastOrder + 1;*/
-	    return this.counter + 1;
+	    console.log(lastOrder + 1);
+	    return lastOrder + 1;
+	    //return this.counter + 1;
 	},
 	addOrder: function(event) {
 	    /* When you click in the map, a click event object is sent as parameter
@@ -40,29 +59,6 @@ const vm = new Vue({
 	     * The click event object contains among other things different
 	     * coordinates that we need when calculating where in the map the click
 	     * actually happened. */
-
-	    /*
-	    let offset = {
-		x: event.currentTarget.getBoundingClientRect().left,
-		y: event.currentTarget.getBoundingClientRect().top,
-	    };*/
-	    
-
-	    socket.emit('addOrder', {
-		orderId: this.getNext(),
-		
-		details: {
-		    /*x: event.clientX - 10 - offset.x,*/
-		    /*y: event.clientY - 10 - offset.y,*/
-		    x: this.order.details.x,
-		    y: this.order.details.y,
-		},
-		orderItems: this.myOrder,
-	    });
-	},
-	displayOrder: function ()
-	{
-	    /*
 	    if (!this.fullname || !this.email || !this.paymentmethod || !this.gender) {
 		this.missingFields = true;
 		
@@ -70,25 +66,29 @@ const vm = new Vue({
 	    } else {
 		this.missingFields = false;
 		
+		
+		this.customerInfoArray = [this.fullname, this.email, this.paymentmethod, this.gender];
+		
+	    }
+	    
+	    socket.emit('addOrder', {orderId: this.getNext(), details: { x: this.localOrder.details.x, y: this.localOrder.details.y,},customerInformation: this.customerInfoArray,orderItems: this.myOrder,});
+	    
+	    console.log(this.orders[0]);
 
-	    */
+	},
+	displayOrder: function ()
+	{
+
 	    let offset = {
 		x: event.currentTarget.getBoundingClientRect().left,
 		y: event.currentTarget.getBoundingClientRect().top,
 	    };
 	    
-	    this.order.details.x = event.clientX - 10 - offset.x;
-	    this.order.details.y = event.clientY - 10 - offset.y;
+	    this.localOrder.details.x = event.clientX - 10 - offset.x;
+	    this.localOrder.details.y = event.clientY - 10 - offset.y;
 	    
 	    
-	    console.log('x: ' + this.order.details.x + ', y: ' + this.order.details.y);
-		
-	    }
-	    
-	    
-	    
-	
+	}
 	
     }
- 
 })
